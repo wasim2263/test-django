09f4e9bf2481ed147a0c12809400eeb093a7b674
wasim2263 <wasim@squaredworks.com>
2018-09-25 01:07:16 +0600

    first commit

---
+++ polls/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Question, Choice
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ("Date information", {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date')
+
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+
